b0705c9a5cc899088cea3b9ca18280004a74908d
Konstatntin <109268327+konstantin-kif@users.noreply.github.com>
2022-10-06 15:15:55 +0700

    hw12

---
+++ hw12.py
@@ -0,0 +1,23 @@
+# Задайте список из вещественных чисел. Напишите программу,
+#  которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# Пример:
+# [1.1, 1.2, 3.1, 5, 10.01] => 0.20
+
+from random import randint
+
+number = int(input('Enter the size of the list: '))
+list = []
+for i in range(number):
+    list.append(float(randint(0, 9)))
+
+min = list[0]
+max = 0
+for i in range(len(list)):
+    if max < list[i]:
+        max = list[i]
+    if min > list[i]:
+        min = list[i]
+difference = (max  - min)
+
+print(list)
+print(float(difference))

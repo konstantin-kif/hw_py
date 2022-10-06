83ee81c0829fe5b64570b602cd1c27b5affa8336
Konstatntin <109268327+konstantin-kif@users.noreply.github.com>
2022-10-06 14:31:29 +0700

    add hw10

--- hw10.py
+++ hw10.py
@@ -3,17 +3,18 @@
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+from random import randint
+
 number = int(input('Enter the size of the list: '))
 list = []
 sum = 0
 for i in range(number):
-    list_number = int(input(f'Enter the number {i+1} '))
-    list.append(list_number)
+    list.append(randint(0, 9))
     if i % 2 != 0:
         sum += list[i]
         
 print(list)
-print(f'Сумма нечетных чисел равна {sum}')
+print(f'The sum of odd numbers is equal to {sum}')
 
 
 

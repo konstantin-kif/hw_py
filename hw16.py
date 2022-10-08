355c3deb49fc904826e7e607805ab768995111b1
Konstatntin <109268327+konstantin-kif@users.noreply.github.com>
2022-10-08 23:59:30 +0700

    factors

--- hw16.py
+++ hw16.py
@@ -6,7 +6,7 @@ factors = []
 for i in range(2, int(number ** 0.5) + 2):
     while number % i == 0:
         factors.append(i)
-        number /= i
+        number //= i
 if number > 1:
     factors.append(int(number))
 print(f'List of prime factors of a number {number}: {factors}')
